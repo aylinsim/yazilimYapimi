a007fc7b5f09d5dfd4f6edfabf7789c5b7cf403b
aylinsim <155490577+aylinsim@users.noreply.github.com>
2024-05-24 23:50:29 +0300

    Update Layout.js

--- word-memorization-game-backend/Layout.js
+++ word-memorization-game-backend/Layout.js
@@ -1,6 +1,13 @@
 
 import React from 'react';
 import { Outlet } from "react-router-dom";
+cd /path/to/your/react-project
+git init
+node_modules
+build
+.env
+git add .
+
 
 function Layout() {
     return (
@@ -9,9 +16,9 @@ function Layout() {
                 <h1 className="header-title"> WORD MEMORIZATION GAME </h1>
                 <div>
                     <div className="navbar">
-                        <button onClick={() => {window.location.href="/quiz"}} className="navbar-button"> Quiz </button>
-                        <button onClick={() => {window.location.href="/stats"}} className="navbar-button"> Stats </button>
-                        <button onClick={() => {window.location.href="/create-question"}} className="navbar-button"> Add Question </button>
+                        <button onClick={() => {window.location.href="/quiz"}} className="navbar-button"> quiz </button>
+                        <button onClick={() => {window.location.href="/stats"}} className="navbar-button"> stats </button>
+                        <button onClick={() => {window.location.href="/create-question"}} className="navbar-button"> add question </button>
                     </div>
                 </div>
 
